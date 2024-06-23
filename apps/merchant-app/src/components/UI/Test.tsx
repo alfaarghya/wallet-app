46b89e85ca26b461e880f9351e3f281ee2e3112d
“alfaarghya” <alfaarghya.dev@gmail.com>
2024-06-23 21:20:21 +0530

    FIX: apps by redefine them 🛠️ || TEST: store package 📦

--- apps/merchant-app/src/components/UI/Test.tsx
+++ apps/merchant-app/src/components/UI/Test.tsx
@@ -1,14 +1,27 @@
+"use client";
 import React from "react";
 import { Test } from "@wallet-app/types/test";
+import { useCountState, useCountValue } from "@wallet-app/store/useCount";
+import { SetterOrUpdater } from "recoil";
 
 const TestUI = () => {
   const abc: Test = {
     name: "alfaarghya_Merchant",
     age: 31,
   };
+  const {
+    value,
+    setValue,
+  }: { value: number; setValue: SetterOrUpdater<number> } = useCountState();
   return (
     <div>
-      Test
+      <div>CountUi {value}</div>
+      <button
+        className="border bg-amber-300 p-2"
+        onClick={() => setValue(value + 1)}
+      >
+        +1
+      </button>
       <div>{abc.name}</div>
     </div>
   );
--- apps/user-app/src/components/UI/Test.tsx
+++ apps/user-app/src/components/UI/Test.tsx
@@ -1,14 +1,28 @@
+"use client";
 import React from "react";
 import { Test } from "@wallet-app/types/test";
+import { useCountState, useCountValue } from "@wallet-app/store/useCount";
+import { SetterOrUpdater } from "recoil";
 
 const TestUI = () => {
+  const {
+    value,
+    setValue,
+  }: { value: number; setValue: SetterOrUpdater<number> } = useCountState();
+  // const count = useCountValue();
   const abc: Test = {
     name: "alfaarghya",
     age: 21,
   };
   return (
     <div>
-      Test
+      <div>CountUi {value}</div>
+      <button
+        className="border bg-amber-300 p-2"
+        onClick={() => setValue(value + 11)}
+      >
+        +11
+      </button>
       <div>{abc.name}</div>
     </div>
   );
